3941840d6efc0e6cb182d297d168e2b33d60fd8b
Jitender Singh <jitender.singh@tothenew.com>
2016-10-24 11:01:46 +0530

    some files deleted

--- client/constants/ActionTypes.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Jitender on 22/10/16.
- */
-
-export const SEND_MESSAGE = 'SEND_MESSAGE';
-export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
-export const MESSAGE_SENT = 'MESSAGE_SENT';
-
